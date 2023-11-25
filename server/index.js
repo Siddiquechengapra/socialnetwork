8690d93e6780e60a87f335551c434d2e05008143
Sid <66302679+Siddiquechengapra@users.noreply.github.com>
2023-11-26 03:02:26 +0530

    Backend completed

--- server/index.js
+++ server/index.js
@@ -12,9 +12,14 @@ import { register } from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
+import { verifyToken } from './middleware/auth.js';
+import { createPost } from './controllers/posts.js';
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename); 
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -39,7 +44,7 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
-// app.post("/posts", verifyToken, upload.single("picture"),  );
+app.post("/posts", verifyToken, upload.single("picture"),createPost  );
 
 /* ROUTES */
 app.use("/auth", authRoutes);
