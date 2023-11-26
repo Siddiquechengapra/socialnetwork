72f68822259a80e53f440c8f5c6a7a263b34d830
Sid <66302679+Siddiquechengapra@users.noreply.github.com>
2023-11-27 02:11:13 +0530

    theme added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
-
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        {/* <ThemeProvider theme={theme}> */}
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+        {/* </ThemeProvider> */}
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
